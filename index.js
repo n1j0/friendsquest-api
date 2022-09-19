d7b7ea7c17219ad39158c689caf46406cffe8f92
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2022-09-19 17:14:01 +0200

    chore(server): disable static file serving

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const pool = new Pool({
 });
 
 app.set('port', (process.env.PORT || 3000))
-app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   pool.connect((err, client, done) => {
