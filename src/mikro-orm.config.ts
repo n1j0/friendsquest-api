63dcb51d17fd1b45943f87034e0af39510e2d87b
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2022-09-20 13:26:45 +0200

    chore(orm): install reflection in order to use ts-morph syntax

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,6 +1,7 @@
 import { config } from 'dotenv'
 import { PostgreSqlDriver } from '@mikro-orm/postgresql'
 import { Configuration, Options } from '@mikro-orm/core'
+import { TsMorphMetadataProvider } from '@mikro-orm/reflection'
 
 if (process.env.NODE_ENV !== 'production') {
     config()
@@ -11,4 +12,5 @@ export default {
     entitiesTs: ['./src/entities'],
     dbName: process.env.DB_DATABASE,
     type: 'postgresql',
+    metadataProvider: TsMorphMetadataProvider,
 } as Options<PostgreSqlDriver> | Configuration<PostgreSqlDriver>
