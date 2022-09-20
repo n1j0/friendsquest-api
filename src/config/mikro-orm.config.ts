47d55af019f449e9a4faf5d855b3c9ca97f533c8
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2022-09-20 14:17:03 +0200

    chore(orm): configure orm migrations and db connection

--- src/config/mikro-orm.config.ts
+++ src/config/mikro-orm.config.ts
@@ -1,6 +1,6 @@
 import { config } from 'dotenv'
 import { PostgreSqlDriver } from '@mikro-orm/postgresql'
-import { Configuration, Options } from '@mikro-orm/core'
+import { Options } from '@mikro-orm/core'
 import { TsMorphMetadataProvider } from '@mikro-orm/reflection'
 
 if (process.env.NODE_ENV !== 'production') {
@@ -8,9 +8,19 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 export default {
+    tsNode: process.env.NODE_ENV !== 'production',
     entities: ['./.out/entities'],
     entitiesTs: ['./src/entities'],
+    migrations: {
+        path: './src/migrations',
+        tableName: 'migrations',
+        transactional: true,
+    },
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     dbName: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: Number.parseInt(process.env.DB_PORT as string, 10) || 5432,
     type: 'postgresql',
     metadataProvider: TsMorphMetadataProvider,
-} as Options<PostgreSqlDriver> | Configuration<PostgreSqlDriver>
+} as Options<PostgreSqlDriver>
