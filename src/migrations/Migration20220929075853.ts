9feb872aec7c6e06dd54ec3367edab5701358e2f
Markus <malmasy.mmt-m2021@fh-salzburg.ac.at>
2022-09-29 10:04:55 +0200

    fix(orm): add migration new migration for username unique
    Relates to FQ-43

---
+++ src/migrations/Migration20220929075853.ts
@@ -0,0 +1,13 @@
+import { Migration } from '@mikro-orm/migrations'
+
+export class Migration20220929075853 extends Migration {
+    async up(): Promise<void> {
+        this.addSql('alter table "user" add constraint "user_username_unique" unique ("username");')
+        this.addSql('alter table "user" add constraint "user_email_unique" unique ("email");')
+    }
+
+    async down(): Promise<void> {
+        this.addSql('alter table "user" drop constraint "user_username_unique";')
+        this.addSql('alter table "user" drop constraint "user_email_unique";')
+    }
+}
