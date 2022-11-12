cc2f9c7cfa4e649d9ca975b631bb0ff2b0fb16cb
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2022-11-12 11:11:47 +0100

    feat(firebase): add possibility to use different users in token endpoint

--- src/routes/_firebaseAuth.ts
+++ src/routes/_firebaseAuth.ts
@@ -8,14 +8,26 @@ const router = express.Router()
  * @openapi
  * /firebase/token:
  *   get:
- *     summary: Receive a firebase token for test user
+ *     summary: Receive a firebase token for a specified test user
+ *     parameters:
+ *       - in: header
+ *         name: userId
+ *         schema:
+ *           type: string
+ *           required: true
+ *           description: Number of the test user
  *     responses:
  *       200:
  *         description: Returns the idToken
  *       500:
  *         description: Internal server error
  */
-router.get('/token', async (_request: Request, response: Response) => {
+router.get('/token', async (request: Request, response: Response) => {
+    const user = request.headers.userId
+    const body = {
+        email: user === '1' ? process.env.FIREBASE_TEST_USER1 : process.env.FIREBASE_TEST_USER2,
+        password: user === '1' ? process.env.FIREBASE_TEST_PASSWORD1 : process.env.FIREBASE_TEST_PASSWORD2,
+    }
     try {
         const token = await fetch(
             // eslint-disable-next-line max-len
@@ -26,8 +38,7 @@ router.get('/token', async (_request: Request, response: Response) => {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-                    email: process.env.FIREBASE_TEST_USER,
-                    password: process.env.FIREBASE_TEST_PASSWORD,
+                    ...body,
                     returnSecureToken: true,
                 }),
             },
