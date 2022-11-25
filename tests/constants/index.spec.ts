ffa277b3c0a78e78a185eef72c98b81503dee0c1
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2022-11-26 00:00:18 +0100

    test(constants): check for correct FriendshipStatus enum values

--- tests/constants/index.spec.ts
+++ tests/constants/index.spec.ts
@@ -4,10 +4,18 @@ describe('constants', () => {
     it('is defined', () => {
         expect(constants).toBeDefined()
     })
+
     it('exports auth header key', () => {
         expect(constants.AUTH_HEADER_KEY).toBeDefined()
     })
+
     it('exports auth header uid', () => {
         expect(constants.AUTH_HEADER_UID).toBeDefined()
+    })
+
+    it('exports enum FriendshipStatus', () => {
+        expect(constants.FriendshipStatus).toBeDefined()
+        expect(constants.FriendshipStatus.ACCEPTED).toBe('accepted')
+        expect(constants.FriendshipStatus.INVITED).toBe('invited')
     })
 })
