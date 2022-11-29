d87ab55b7a97160b88cfb7825eb9676559776f65
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2022-11-29 15:35:56 +0100

    refactor(types): use RouterInterface for Route "routerClass" ReturnType

--- src/types/routes.ts
+++ src/types/routes.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express'
 import { ORM } from '../orm.js'
+import { RouterInterface } from '../routes/routerInterface'
 
 export interface Route {
     path: string
     // eslint-disable-next-line no-unused-vars
-    routerClass: new (argument0: Router, argument1: ORM) => {
-        createAndReturnRoutes: () => Router,
-    },
+    routerClass: new (argument0: Router, argument1: ORM) => RouterInterface
 }
