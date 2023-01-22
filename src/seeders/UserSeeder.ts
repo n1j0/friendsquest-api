fb0b032a8e287d3417048b8ca38120e386306a96
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2023-01-22 14:43:53 +0100

    chore(seeder): add new user of staging system

--- src/seeders/UserSeeder.ts
+++ src/seeders/UserSeeder.ts
@@ -4,15 +4,15 @@ import { User } from '../entities/user.js'
 
 export class UserSeeder extends Seeder {
     async run(em: EntityManager, context: Dictionary): Promise<void> {
-        context['smHKdjP1VEP11HZUMUmD7h5DnY82'] = em.create(User, {
+        context['Uj0Snu6MePcpWJ2dBmNGEdUqt8n2'] = em.create(User, {
             email: 'test@test.at',
-            uid: 'smHKdjP1VEP11HZUMUmD7h5DnY82',
+            uid: 'Uj0Snu6MePcpWJ2dBmNGEdUqt8n2',
             username: 'peterPan',
             friendsCode: '00000',
         } as RequiredEntityData<User>)
-        context['NFT7IF5F46eLuzKnjuLX4ZmWLTB3'] = em.create(User, {
+        context['RqWiQwRApsWosyfZOoSBttk2rn12'] = em.create(User, {
             email: 'test@test2.at',
-            uid: 'NFT7IF5F46eLuzKnjuLX4ZmWLTB3',
+            uid: 'RqWiQwRApsWosyfZOoSBttk2rn12',
             username: 'lauraPan',
             friendsCode: '00001',
         } as RequiredEntityData<User>)
