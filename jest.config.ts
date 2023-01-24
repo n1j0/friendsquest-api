2ede0cd733ef266f388e7d139ecf9032ae3bced8
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2023-01-24 22:45:44 +0100

    fix(jest): remove unnecessary single quotes

--- jest.config.ts
+++ jest.config.ts
@@ -25,10 +25,10 @@ const jestSetup: JestConfigWithTsJest = {
     ],
     coverageThreshold: {
         global: {
-            'branches': 80,
-            'functions': 80,
-            'lines': 80,
-            'statements': 80,
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
         },
     },
     setupFiles: ['<rootDir>/jest.setup.ts'],
