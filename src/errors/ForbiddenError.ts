5663d647eefb0ab1561cf05bf1fd00f637105b3a
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2023-01-28 01:56:46 +0100

    ichore(eslint): remove unused eslint-disable comments or fix them

--- src/errors/ForbiddenError.ts
+++ src/errors/ForbiddenError.ts
@@ -3,7 +3,6 @@ import { Mapper } from './ProblemDocument.js'
 import { ProblemDocument } from '../types/problemDocument'
 
 export class ForbiddenError extends Error {
-    // eslint-disable-next-line no-unused-vars
     static getErrorDocument(ErrorString?: string): ProblemDocument {
         return Mapper.mapError({
             detail: `${(ErrorString)}`,
