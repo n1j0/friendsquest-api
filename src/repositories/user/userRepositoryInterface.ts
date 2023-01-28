9c2e4af50c1729b96cf6020a1ad6690ed6f669e5
Markus <malmasy.mmt-m2021@fh-salzburg.ac.at>
2023-01-28 15:58:38 +0100

    refactor(user): change signature of updateUser

--- src/repositories/user/userRepositoryInterface.ts
+++ src/repositories/user/userRepositoryInterface.ts
@@ -7,7 +7,8 @@ export interface UserRepositoryInterface {
     getUserByFriendsCode(friendsCode: number | string): Promise<User>
     getAllUsers(): Promise<object[]>
     createUser(user: User): Promise<User>
-    updateUser(id: number | string, userData: any): Promise<{ user: User, points: number }>
+    updateUser(id: number | string,
+               userData: { username: string, email: string }): Promise<{ user: User, points: number }>
     deleteUser(id: number | string): Promise<void>
     addPoints(uid: string, points: number): Promise<User>
 }
