5ffa3f860f60c9fee931d8296e9a09c49a7a6391
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2023-01-30 17:40:49 +0100

    fix(CHECKS): create additional health route with plain text
    The current version of dokku doesn't support json for CHECKS files.

---
+++ tests/config/actuatorConfig.spec.ts
@@ -0,0 +1,21 @@
+import { Request } from 'express'
+import { actuatorConfig } from '../../src/config/actuatorConfig'
+import responseMock from '../test-helper/responseMock'
+
+describe('actuatorConfig', () => {
+    it('export custom health route with plain "UP" text', () => {
+        const response = responseMock
+        const config = actuatorConfig
+        const { customEndpoints } = config
+
+        expect(Array.isArray(customEndpoints)).toBe(true)
+        expect(customEndpoints[0]).toStrictEqual({
+            id: 'health-plain',
+            controller: expect.any(Function),
+        })
+
+        customEndpoints[0].controller({} as unknown as Request, response)
+        expect(response.status).toHaveBeenCalledWith(200)
+        expect(response.send).toHaveBeenCalledWith('UP')
+    })
+})
