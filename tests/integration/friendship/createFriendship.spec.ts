e395d77c0da8b33d2a9c52db1cef3010394606aa
Markus <malmasy.mmt-m2021@fh-salzburg.ac.at>
2023-02-02 15:18:34 +0100

    tests(integration): add integration tests

---
+++ tests/integration/friendship/createFriendship.spec.ts
@@ -0,0 +1,5 @@
+describe('CreateFriendship', () => {
+    it('should return 201', async () => {})
+
+    it('should return error', async () => {})
+})
---
+++ tests/integration/user/createUser.spec.ts
@@ -0,0 +1,5 @@
+describe('CreateUser', () => {
+    it('should return 201', async () => {})
+
+    it('should return error', async () => {})
+})
